a87881db802d150aa059641d3bbf58f493447e36
Kanishka Malakar <malakarkanishka@gmail.com>
2026-09-02 00:32:49 +0530

    Export FDIR-002 sensor health monitor

--- radiant/fdir/__init__.py
+++ radiant/fdir/__init__.py
@@ -1,5 +1,21 @@
 """Fault detection, isolation and recovery primitives for RADIANT-DAQ."""
 
 from .transport import IntegrityFinding, IntegrityReport, TransportIntegrityMonitor
+from .sensor import (
+    SensorHealthConfig,
+    SensorFinding,
+    ChannelHealth,
+    SensorHealthReport,
+    SensorHealthMonitor,
+)
 
-__all__ = ["IntegrityFinding", "IntegrityReport", "TransportIntegrityMonitor"]
+__all__ = [
+    "IntegrityFinding",
+    "IntegrityReport",
+    "TransportIntegrityMonitor",
+    "SensorHealthConfig",
+    "SensorFinding",
+    "ChannelHealth",
+    "SensorHealthReport",
+    "SensorHealthMonitor",
+]
