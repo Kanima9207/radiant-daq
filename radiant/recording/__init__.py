a8a08a20d819cbc5a0f5d5b45969a5a36c6bdef7
Kanishka Malakar <malakarkanishka@gmail.com>
2026-09-01 23:58:09 +0530

    feat(recording): export flight recorder

--- radiant/recording/__init__.py
+++ radiant/recording/__init__.py
@@ -1,5 +1,6 @@
 """Persistent event-recording primitives for RADIANT-DAQ."""
 
 from .event_record import EventRecord
+from .flight_recorder import FlightRecorder
 
-__all__ = ["EventRecord"]
+__all__ = ["EventRecord", "FlightRecorder"]
