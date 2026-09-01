d57c286a75ac8b6a0038e106fbe854fa59dd0f0a
Kanishka Malakar <malakarkanishka@gmail.com>
2026-09-02 00:04:16 +0530

    Export TIMING-002 synchronization utilities

--- radiant/timing/__init__.py
+++ radiant/timing/__init__.py
@@ -1,5 +1,15 @@
 """Timing models and synchronization primitives for RADIANT-DAQ."""
 
 from .clock import LocalClock
+from .synchronization import (
+    SynchronizationEstimate,
+    estimate_synchronization,
+    synchronization_error,
+)
 
-__all__ = ["LocalClock"]
+__all__ = [
+    "LocalClock",
+    "SynchronizationEstimate",
+    "estimate_synchronization",
+    "synchronization_error",
+]
